dd4e705f3cfda4e2fe25b4ef0369f69ab45db1b2
Joshua Adam George <132799529+xJAGx1987x@users.noreply.github.com>
2023-05-08 10:57:29 -0400

    Update/Tidying
    Cleaned up the logic within convertAll. Accounted for equal input/output first. Cleaned up if-else- if block within the same method. 
    
    Added Boolean method to determine if the input value is less that absolute zero.

--- TempConverter.java
+++ TempConverter.java
@@ -1,42 +1,39 @@
 public class TempConverter {
 
     public static double convertAll(String inString, String outString, double temp ){
-        double result = 0 ;
 
-        public static double convertAll(String inString, String outString, double temp ){
+        double result;
 
-            double result;
-            
-            if (inString.equalsIgnoreCase(outString)) {
-                return temp;
-            }
-            if (inString.equalsIgnoreCase("kelvin")) {
-                if (outString.equalsIgnoreCase("celsius")) {
-                    result = kelvinToCelsius(temp);
-                }
-                else {
-                    result = kelvinToFahr(temp);
-                }
-            }
-            else if (inString.equalsIgnoreCase("celsius")) {
-                if (outString.equalsIgnoreCase("kelvin")) {
-                    result = celsiusToKelvin(temp);
-                }
-                else {
-                    result = celsiusToFahr(temp);
-                }
+        if (inString.equalsIgnoreCase(outString)) {
+            return temp;
+        }
+
+        if (inString.equalsIgnoreCase("kelvin")) {
+            if (outString.equalsIgnoreCase("celsius")) {
+                result = kelvinToCelsius(temp);
             }
             else {
-                if (outString.equalsIgnoreCase("celsius")) {
-                    result = fahrToCelsius(temp);
-                }
-                else {
-                    result = fahrToKelvin(temp);
-                }
+                result = kelvinToFahr(temp);
             }
-            return result ;
         }
-    }
+        else if (inString.equalsIgnoreCase("celsius")) {
+            if (outString.equalsIgnoreCase("kelvin")) {
+                result = celsiusToKelvin(temp);
+            }
+            else {
+                result = celsiusToFahr(temp);
+            }
+        }
+        else {
+            if (outString.equalsIgnoreCase("celsius")) {
+                result = fahrToCelsius(temp);
+            }
+            else {
+                result = fahrToKelvin(temp);
+            }
+        }
+        return result ;
+    } // End convertAll
 
     public static double celsiusToFahr( double t ){
         return ( ( t * 1.8 ) + 32 );
@@ -61,6 +58,10 @@ public class TempConverter {
 
     public static double kelvinToFahr( double t ){
         return ( ( t * 1.8) - 459.67 ) ;
+    }
+
+    public static boolean isValidTemp(String inString, double temp) {
+        return convertAll(inString, "kelvin", temp) >= 0;
     }
 
 }
